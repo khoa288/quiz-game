f86c63bee458672105bde4e6907fdf82b49ecdc4
Khoa Nguyen <nguyenminhkhoa77284@gmail.com>
2023-12-01 14:46:32 +0700

    feat: displays a list of high scores at the end of the game

--- quiz_game.py
+++ quiz_game.py
@@ -1,8 +1,9 @@
 import csv
 import random
+import os
 
 
-def load_questions(filename: str, number_of_questions=10: int) -> list:
+def load_questions(filename: str, number_of_questions: int = 10) -> list:
     with open(filename, "r") as csvfile:
         csvreader = csv.reader(csvfile)
 
@@ -26,13 +27,38 @@ def ask_question(question: list) -> bool:
         return False
 
 
+def save_score(name: str, score: int) -> None:
+    with open("high_scores.csv", "a") as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow([name, score])
+
+
+def display_high_scores() -> None:
+    if not os.path.exists("high_scores.csv"):
+        return
+
+    with open("high_scores.csv", "r") as csvfile:
+        csvreader = csv.reader(csvfile)
+        high_scores = sorted(csvreader, key=lambda x: int(x[1]), reverse=True)
+
+    print("\nHigh Scores:")
+    for name, score in high_scores[:10]:
+        print(f"{name}: {score}")
+
+
 def run_quiz() -> None:
     questions = load_questions("quiz_questions.csv")
     score = 0
     for question in questions:
         if ask_question(question):
             score += 1
+ 
+    name = input("Enter your name to save your score: ")
+    if name.strip():
+        save_score(name, score)
+
     print(f"You scored {score}/{len(questions)}")
+    display_high_scores()
 
 
 if __name__ == "__main__":
